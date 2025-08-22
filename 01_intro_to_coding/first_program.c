5578ee1a078713e6a7f75480d2671d57abb77e45
tcockerham22 <tcockerham3539@floridapoly.edu>
2025-08-22 14:36:10 +0000

    Created the first program

---
+++ 01_intro_to_coding/first_program.c
@@ -0,0 +1,24 @@
+/*
+
+Multi-line comment.
+08/20/2025
+Ty Cockerham
+The first program in C.
+
+*/
+
+// one line comment
+
+// Preprocessor directive
+// stdio.h - standard input output
+// .h - header file
+#include <stdio.h>      
+
+// Create the main function
+int main(void) {
+    printf("Hello\nWorld\n"); // \n (new line) - escape sequence
+
+    printf("I\nam\nready\nto\ncode!\n");
+
+    return 0;
+}
